5a63f545dabe294b85101005ee6b0407dca77878
Boris Bera <Boris.Bera@wolterskluwer.com>
2013-11-13 09:10:49 -0500

    Rendering block from the hook on the welcome page

--- lib/featured_projects/hooks.rb
+++ lib/featured_projects/hooks.rb
@@ -1,7 +1,10 @@
 module FeaturedProjects
   class Hooks < Redmine::Hook::ViewListener
-    # :project
-    # :form
     render_on :view_projects_form, :partial => 'hooks/projects/featured_project_fields'
+
+    def view_welcome_index_right(context)
+      featured_projects = Project.featured.all
+      context[:hook_caller].render :partial => 'hooks/welcome/featured_projects', :locals => { :featured_projects => featured_projects }
+    end
   end
 end
